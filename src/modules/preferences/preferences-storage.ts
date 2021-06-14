772396aba684453527a019eaeec603cc801ff3a6
Allan Juan <allanjuansil@gmail.com>
2021-06-14 20:13:48 -0300

    feat: implement preferences storage

---
+++ src/modules/preferences/preferences-storage.ts
@@ -0,0 +1,36 @@
+import * as fs from 'fs-extra'
+import * as path from 'path'
+
+export class PreferencesStorage {
+  private configFilePath: string
+
+  private configObject: Record<string, string> = {}
+
+  constructor(configDirectory: string) {
+    this.configFilePath = path.join(configDirectory, 'config.json')
+  }
+
+  async get(key: string): Promise<string> {
+    const userConfig = await this.getConfigObject()
+    return userConfig[key]
+  }
+
+  async set(key: string, value: string): Promise<void> {
+    const userConfig = await this.getConfigObject()
+    userConfig[key] = value
+
+    return this.writeConfigToFileSystem()
+  }
+
+  private async getConfigObject() {
+    if (!this.configObject) {
+      this.configObject = await fs.readJSON(this.configFilePath)
+    }
+
+    return this.configObject || {}
+  }
+
+  private writeConfigToFileSystem() {
+    return fs.writeJson(this.configFilePath, this.configObject)
+  }
+}
