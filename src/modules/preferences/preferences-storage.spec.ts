753512ac500f36d2788b9cad46595b2bdcd6d72a
Allan Juan <allanjuansil@gmail.com>
2021-06-18 20:12:49 -0300

    chore: use sinon in PreferencesStorage test

--- src/modules/preferences/preferences-storage.spec.ts
+++ src/modules/preferences/preferences-storage.spec.ts
@@ -1,28 +1,28 @@
+import sinon, { SinonSpy } from 'sinon'
 import { expect, fancy } from 'fancy-test'
 import { PreferencesStorage } from './preferences-storage'
 import { IFileSystem } from './file-system'
 
 let storage: PreferencesStorage
 let fileSystem: MockFileSystem
+let writeSpy: SinonSpy
 
 class MockFileSystem implements IFileSystem {
-  writeJSONCalled = false
-
   async readJSON() {
     return { lang: 'pt-BR' }
   }
 
-  async writeJSON(path: string, object: any) {
-    this.writeJSONCalled = true
-  }
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  async writeJSON(_: string, __: any) {}
 }
 
-function initStorage() {
+function setup() {
   fileSystem = new MockFileSystem()
   storage = new PreferencesStorage(fileSystem)
+  writeSpy = sinon.spy(fileSystem, 'writeJSON')
 }
 
-const test = fancy.do(initStorage)
+const test = fancy.do(setup)
 
 describe('PreferencesStorage', () => {
   test.it('should read JSON from file system', async () => {
@@ -32,6 +32,6 @@ describe('PreferencesStorage', () => {
 
   test.it('should call write JSON on set', async () => {
     await storage.set('lang', 'en-US')
-    expect(fileSystem.writeJSONCalled).to.be.true
+    expect(writeSpy.firstCall.lastArg).to.eql({ lang: 'en-US' })
   })
 })
