e02c333d8c9d2ed581d2ccff7fb073227a4d5120
Allan Juan <allanjuansil@gmail.com>
2021-06-19 09:55:29 -0300

    Rewrite GetPreference tests

--- src/modules/preferences/use-cases/get-preference-use-case.spec.ts
+++ src/modules/preferences/use-cases/get-preference-use-case.spec.ts
@@ -1,4 +1,4 @@
-import { expect } from 'chai'
+import { expect, fancy } from 'fancy-test'
 import { MockPreferenceStorage } from '../preferences-storage'
 import { GetPreferenceUseCase } from './get-preference-use-case'
 
@@ -6,16 +6,16 @@ describe('GetPreferenceUseCase', () => {
   let storage: MockPreferenceStorage
   let getPreference: GetPreferenceUseCase
 
-  beforeEach(() => {
+  const test = fancy.do(() => {
     storage = new MockPreferenceStorage()
     getPreference = new GetPreferenceUseCase(storage)
   })
 
-  it('should return a falsy value if there is no value set', () => {
+  test.it('should return a falsy value if there is no value set', () => {
     expect(getPreference.run('lang')).to.eventually.not.be.ok
   })
 
-  it('should return a value if there is one', () => {
+  test.it('should return a value if there is one', () => {
     storage.set('lang', 'pt-BR')
     expect(getPreference.run('lang')).to.eventually.equal('pt-BR')
   })
