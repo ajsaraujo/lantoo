4d36ca72a8bc14bfdced376bcc81d61096b492b3
Allan Juan <allanjuansil@gmail.com>
2021-06-20 09:49:23 -0300

    Extract fixCasing to i18n

--- src/modules/i18n/i18n.ts
+++ src/modules/i18n/i18n.ts
@@ -1,7 +1,21 @@
+import { inject, injectable } from 'tsyringe'
+import { FuzzyFinder } from './fuzzy-finder'
 import validLanguages from './valid-languages'
 
+@injectable()
 export class I18n {
+  constructor(@inject('FuzzyFinder') private fuzzy: FuzzyFinder) {}
+
   isLanguageCode(str: string) {
     return validLanguages.includes(str)
+  }
+
+  public fixCasing(languageCode: string) {
+    if (languageCode.includes('-')) {
+      const [firstPart, secondPart] = languageCode.split('-')
+      return `${firstPart.toLowerCase()}-${secondPart.toUpperCase()}`
+    }
+
+    return languageCode.toLowerCase()
   }
 }
