e2d6be19217e2e3eb897cada845c41180dab5def
Allan Juan <allanjuansil@gmail.com>
2021-07-03 11:31:49 -0300

    improve IFileSystem signature

--- src/modules/preferences/storage/file-system.ts
+++ src/modules/preferences/storage/file-system.ts
@@ -1,16 +1,16 @@
-import * as fs from 'fs-extra';
+import * as fs from 'fs-extra'
 
 export interface IFileSystem {
-	readJSON(path: string): Promise<any>
-	writeJSON(path: string, object: any): Promise<any>
+  readJSON(path: string): Promise<unknown[] | Record<string, unknown>>
+  writeJSON(path: string, object: any): Promise<void>
 }
 
 export class FileSystem implements IFileSystem {
-	readJSON(path: string): Promise<any> {
-		return fs.readJSON(path);
-	}
+  readJSON(path: string) {
+    return fs.readJSON(path)
+  }
 
-	writeJSON(path: string, object: [] | Record<string, unknown>): Promise<any> {
-		return fs.writeJSON(path, object);
-	}
+  writeJSON(path: string, object: any) {
+    return fs.writeJSON(path, object)
+  }
 }
--- src/modules/preferences/storage/preferences-storage.ts
+++ src/modules/preferences/storage/preferences-storage.ts
@@ -1,103 +1,105 @@
-import * as path from 'path';
-import assert from 'assert';
+import * as path from 'path'
+import assert from 'assert'
 
-import { FileSystem, IFileSystem } from './file-system';
+import { FileSystem, IFileSystem } from './file-system'
 
 export interface IPreferencesStorage {
-	get(key: string): Promise<string>
-	set(key: string, value: string): Promise<void>
-	configDirectory: string
+  get(key: string): Promise<string>
+  set(key: string, value: string): Promise<void>
+  configDirectory: string
 }
 
 export class PreferencesStorage implements IPreferencesStorage {
-	private configFilePath = ''
+  private configFilePath = ''
 
-	private configObject: Record<string, string> = {}
+  private configObject: Record<string, string> = {}
 
-	private configObjectWasLoaded = false
+  private configObjectWasLoaded = false
 
-	constructor(private fs: IFileSystem = new FileSystem()) {
-		//
-	}
+  constructor(private fs: IFileSystem = new FileSystem()) {
+    //
+  }
 
-	set configDirectory(directory: string) {
-		this.configFilePath = path.join(directory, 'config.json');
-	}
+  set configDirectory(directory: string) {
+    this.configFilePath = path.join(directory, 'config.json')
+  }
 
-	async get(key: string): Promise<string> {
-		this.assertConfigFilePathIsDefined();
+  async get(key: string): Promise<string> {
+    this.assertConfigFilePathIsDefined()
 
-		await this.getConfigObject();
+    await this.getConfigObject()
 
-		return this.configObject[key];
-	}
+    return this.configObject[key]
+  }
 
-	async set(key: string, value: string): Promise<void> {
-		this.assertConfigFilePathIsDefined();
+  async set(key: string, value: string): Promise<void> {
+    this.assertConfigFilePathIsDefined()
 
-		await this.getConfigObject();
+    await this.getConfigObject()
 
-		this.configObject[key] = value;
+    this.configObject[key] = value
 
-		await this.writeConfigToFileSystem();
-	}
+    await this.writeConfigToFileSystem()
+  }
 
-	private async getConfigObject() {
-		if (this.configObjectWasLoaded) {
-			return;
-		}
+  private async getConfigObject() {
+    if (this.configObjectWasLoaded) {
+      return
+    }
 
-		await this.loadConfigObject();
+    await this.loadConfigObject()
 
-		this.configObjectWasLoaded = true;
-	}
+    this.configObjectWasLoaded = true
+  }
 
-	private async loadConfigObject() {
-		try {
-			this.configObject = await this.fs.readJSON(this.configFilePath);
-		} catch (error) {
-			await this.handleReadJSONError(error);
-		}
-	}
+  private async loadConfigObject() {
+    try {
+      this.configObject = (await this.fs.readJSON(
+        this.configFilePath
+      )) as Record<string, string>
+    } catch (error) {
+      await this.handleReadJSONError(error)
+    }
+  }
 
-	private async handleReadJSONError(error: any) {
-		const configFileNotFound = error.code === 'ENOENT';
+  private async handleReadJSONError(error: any) {
+    const configFileNotFound = error.code === 'ENOENT'
 
-		if (configFileNotFound) {
-			await this.createEmpyConfigFile();
-		} else {
-			throw error;
-		}
-	}
+    if (configFileNotFound) {
+      await this.createEmptyConfigFile()
+    } else {
+      throw error
+    }
+  }
 
-	private async createEmpyConfigFile() {
-		await this.fs.writeJSON(this.configFilePath, {});
-		this.configObject = {};
-	}
+  private async createEmptyConfigFile() {
+    await this.fs.writeJSON(this.configFilePath, {})
+    this.configObject = {}
+  }
 
-	private async writeConfigToFileSystem() {
-		await this.fs.writeJSON(this.configFilePath, this.configObject);
-	}
+  private async writeConfigToFileSystem() {
+    await this.fs.writeJSON(this.configFilePath, this.configObject)
+  }
 
-	private assertConfigFilePathIsDefined() {
-		assert(this.configFilePath, 'Config file path is not defined');
-	}
+  private assertConfigFilePathIsDefined() {
+    assert(this.configFilePath, 'Config file path is not defined')
+  }
 }
 
 export class MockPreferenceStorage implements IPreferencesStorage {
-	configDirectory = ''
+  configDirectory = ''
 
-	private preferencesObject: { [key: string]: string } = {}
+  private preferencesObject: { [key: string]: string } = {}
 
-	async get(key: string): Promise<string> {
-		return this.preferencesObject[key];
-	}
+  async get(key: string): Promise<string> {
+    return this.preferencesObject[key]
+  }
 
-	async set(key: string, value: string): Promise<void> {
-		this.preferencesObject[key] = value;
-	}
+  async set(key: string, value: string): Promise<void> {
+    this.preferencesObject[key] = value
+  }
 
-	clear(): void {
-		this.preferencesObject = {};
-	}
+  clear(): void {
+    this.preferencesObject = {}
+  }
 }
