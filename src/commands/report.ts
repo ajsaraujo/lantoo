351fc2d4c0a6ec02b3f16df25878bd0b4994c8b0
Allan Juan <allanjuansil@gmail.com>
2021-08-14 11:22:59 -0300

    Refactor report formatting

--- src/commands/report.ts
+++ src/commands/report.ts
@@ -30,10 +30,10 @@ export default class Report extends Command {
 		let index = 0
 
 		cli.table(progressReports, {
-			index: { header: '#', get: () => this.alignRight(index++, '99'.length) },
+			index: this.rightAlignedColumn({ header: '#', get: () => index++, maxValueLen: '99'.length }),
 			language: { header: 'Lang' },
-			translationKeys: { header: 'Keys translated', get: (row) => this.alignRight(row.translatedStrings, String(TranslationProgress.stringsKnown)) },
-			'%': { get: (row) => this.formatPercentage(row.percentageOfStringsTranslated) },
+			translationKeys: this.rightAlignedColumn({ header: 'Keys translated', get: (row) => row.translatedStrings, maxValueLen: String(TranslationProgress.stringsKnown).length }),
+			'%': this.rightAlignedColumn({ header: '%', get: (row) => this.formatPercentage(row.percentageOfStringsTranslated), maxValueLen: '100.00'.length }),
 		})
 	}
 
@@ -45,15 +45,17 @@ export default class Report extends Command {
 		return progressReports.sort((a, b) => b.translatedStrings - a.translatedStrings)
 	}
 
-	private alignRight(value: string | number, maxLen: number | string) {
-		const str = String(value)
-		const padding = typeof maxLen === 'number' ? maxLen : maxLen.length
+	private rightAlignedColumn(options: { header: string, get: (tp: TranslationProgress) => string | number, maxValueLen: number }) {
+		const { header, get, maxValueLen } = options
+		const padding = Math.max(header.length, maxValueLen)
 
-		return str.padStart(padding, ' ')
+		return {
+			header: header.padStart(padding, ' '),
+			get: (row: TranslationProgress) => String(get(row)).padStart(padding, ' '),
+		}
 	}
 
 	private formatPercentage(percentage: number) {
-		const twoDigits = (percentage * 100).toFixed(2)
-		return this.alignRight(twoDigits, '100.00')
+		return (percentage * 100).toFixed(2)
 	}
 }
